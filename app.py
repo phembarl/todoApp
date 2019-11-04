6932763239966c10733e2ba3406867c950245a69
Femi Balogun <phembarl@yahoo.com>
2019-11-04 21:02:24 +0100

    user inputs

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    body = {}
+    error = False
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description = description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
+
 
 @app.route('/')
 def index():
-    render_template('index.html', data = [
-        {
-            'description': 'Todo 1'
-        },
-        {
-            'description': 'Todo 2'
-        },
-        {
-            'description': 'Todo 3'
-        },
-    ])
+    return render_template('index.html', data=Todo.query.all())
+
+if '__main__':
+    app.run()
